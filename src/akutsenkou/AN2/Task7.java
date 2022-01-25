1e5d866b3b54372a76f6ce2a5c7b33758efe9f5a
Andreykutsenkou <98279478+Andreykutsenkou@users.noreply.github.com>
2022-01-26 00:52:55 +0300

    additional task form AN2.pdf

---
+++ src/akutsenkou/AN2/Task7.java
@@ -0,0 +1,26 @@
+package akutsenkou.AN2;
+/*
+В переменную записываете количество программистов. В
+зависимости от количества программистов необходимо вывести правильно
+окончание. Например:
+• 2 программиста
+• 1 программиста
+• 10 программистов
+• и т.д.
+ */
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfDev;
+        System.out.print("Введите количество программистов: ");
+        numberOfDev = sc.nextInt();
+        if(numberOfDev == 1 || numberOfDev%10 == 1) System.out.println(numberOfDev + " программист");
+            else if (numberOfDev == 2 || numberOfDev%10 == 2 || numberOfDev == 3 || numberOfDev%10 == 3 || numberOfDev == 4 || numberOfDev%10 == 4)
+                System.out.println(numberOfDev + " программиста");
+                else System.out.println(numberOfDev + " программистов");
+
+    }
+}
