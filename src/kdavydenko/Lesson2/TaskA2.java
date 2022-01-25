9cbf534533b83ad5c08c0e2c33826916749b4c7f
Николай Давыденко <davydenko.kolya@bk.ru>
2022-01-25 21:15:10 +0300

    TaskA2

---
+++ src/kdavydenko/Lesson2/TaskA2.java
@@ -0,0 +1,15 @@
+package kdavydenko.Lesson2;
+
+import java.util.Arrays;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        String a, b, c, d;
+        a = "Твои права на славу очень хрупки,";
+        b = "И если вычесть из заслуг";
+        c = "Ошибки юности и поздних лет уступки,—";
+        d = "Пиши пропало, милый друг.";
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println(a + '\n' + b + '\n' + c + '\n' + d);
+    }
+}
