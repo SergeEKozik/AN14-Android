83a2364326058eb07d1ec254e5e5081fd6029905
Andreykutsenkou <98279478+Andreykutsenkou@users.noreply.github.com>
2022-01-26 00:09:23 +0300

    additional task form AN2.pdf

---
+++ src/akutsenkou/AN2/Task3.java
@@ -0,0 +1,24 @@
+package akutsenkou.AN2;
+/*
+Дано целое число. Если оно является положительным, то прибавить к нему 1.
+Если отрицательным, то вычесть из него 2. Если нулевым, то заменить его на
+10. Вывести полученное число.
+ */
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        System.out.print("Введите целое число: ");
+        number = sc.nextInt();
+        if(number > 0) System.out.println("Число положительное, прибавляем к нему единицу, число равно " + (++number));
+            else if (number < 0) System.out.println("Число отрицательное, отнимаем от числа две единицы, число равно: " + (number -=2));
+                else {
+                    number = 10;
+                    System.out.println("Число равно нулю, меняем его на число 10, число равно: " + number);
+        }
+
+    }
+}
