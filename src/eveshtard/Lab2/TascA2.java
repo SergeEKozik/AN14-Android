f6d42dd68f3cbd5ebb17b150a1c4b8bacfb222df
ElenaVeshtard <h7692306@yandex.ru>
2022-01-25 16:06:09 +0300

    Lab2: Tasks A2, B1

---
+++ src/eveshtard/Lab2/TascA2.java
@@ -0,0 +1,13 @@
+package eveshtard.Lab2;
+
+public class TascA2 {
+    public static void main(String[] args) {
+        System.out.println("Моё любимое стихотворение\n");
+
+        String a = "У лукоморья дуб зелёный";
+        String b = "Златая цепь на дубе том";
+        String c = "И днём и ночью кот учёный";
+        String d = "Всё ходит по цепи кругом.";
+        System.out.println(a + "\n" + "\n" + b + "\n" + "\n" + c + "\n" + "\n" + d);
+    }
+}
