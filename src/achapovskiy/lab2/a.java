4b5886aef5a832f661f4bc1d58415cbbbd48583f
ChapovskiyArtsiom <98259437+ChapovskiyArtsiom@users.noreply.github.com>
2022-01-25 19:17:51 +0300

    lab2 Task A 1-2

--- src/achapovskiy/lab2/a.java
+++
@@ -1,7 +0,0 @@
-package achapovskiy.lab2;
-
-public class a {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/achapovskiy/lab2/a/TaskA2.java
@@ -0,0 +1,11 @@
+package achapovskiy.lab2.a;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        String a = "Ты говоришь: «Она не стоит свеч, ";
+        String b = "Игра судьбы. Темны её сплетенья». ";
+        String c = "Но не бывает лишних встреч,";
+        String d = "И неслучайны совпаденья…";
+        System.out.println("Моё любимое стихотворение: \n" + a +"\n"+ b+ "\n" + c +"\n" + d );
+    }
+}
