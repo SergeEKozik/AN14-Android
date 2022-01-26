7306b1ea4ca0b88129fc001519da77bbae4b4c11
Alexandr Kalevich <98311174+AleksandrKalevich@users.noreply.github.com>
2022-01-27 01:30:53 +0300

    Task B1 completed

--- src/akalevich/Lesson_2/Task_B1.java
+++ src/akalevich/Lesson_2/Task_B1.java
@@ -3,12 +3,13 @@ package akalevich.Lesson_2;
 import static java.lang.Integer.parseInt;
 
 public class Task_B1 {
-    public static void main(String[] args)
-    {
-        int d= parseInt("111" , 10);
-        byte b=(byte) 111;
-        long o;
-        int h;
-        float f;
+    public static void main(String[] args) {
+        int d = 111;
+        byte b = 0b1101111;
+        long o = 0157;
+        double h = 0x1.bc71a9fbe76c9p6;
+        float f = 111.111f;
+        System.out.println(d + " " + b + " " + o + " " + h + " " + f);
+        //System.out.printf("%d ",d + "%b ",b + "%o ",o + "%h ",h + "%f ",f);
     }
 }
