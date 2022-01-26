b82c20acb525f5192516a47eebd0ed417e0bfb24
ChapovskiyArtsiom <98259437+ChapovskiyArtsiom@users.noreply.github.com>
2022-01-26 13:51:55 +0300

    lab2 Task B 1-2

---
+++ src/achapovskiy/lab2/b/TaskB1.java
@@ -0,0 +1,12 @@
+package achapovskiy.lab2.b;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        double d = 111.111;   //возможно ошибка в условии, вместо int должно быть double
+        byte b = 0b111;
+        long o = 0111;
+        int h = 0x111;
+        float f = 111.111f;
+        System.out.println(d +" " + b +" " + o +" " + h +" " + f );
+    }
+}
