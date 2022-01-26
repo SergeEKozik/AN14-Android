a0ce5bc6b248825732b892101b9554b17dac43ee
ElenaVeshtard <h7692306@yandex.ru>
2022-01-26 12:35:13 +0300

    Lab2: Tasks A2, B2

---
+++ src/eveshtard/Lab2/TaskB1.java
@@ -0,0 +1,16 @@
+package eveshtard.Lab2;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        int d= 111;
+        byte b = 0b111;
+        long o = 0111;
+        int h= 0x111;
+        float f= 111.111f;
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(o);
+        System.out.println(h);
+        System.out.println(f);
+    }
+}
