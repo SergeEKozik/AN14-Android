dc9d9d6681792222262dda270150931017bbe1bd
Kirill <Kirill@.com>
2022-01-27 18:11:08 +0300

    task c1

---
+++ src/kvizhunou/Lab2/TaskC1.java
@@ -0,0 +1,17 @@
+package kvizhunou.Lab2;
+
+import java.util.Scanner;
+
+public class TaskC1 {
+   public static double convertCelsiumToForenheit(int celsium){
+return  celsium*9/5+32;
+
+   }
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Введите градусы цельсия");
+        int celsium=in.nextInt();
+        System.out.println(convertCelsiumToForenheit(celsium));
+    }
+}
