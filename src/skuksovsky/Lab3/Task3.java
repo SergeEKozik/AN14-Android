1d25e124ed3aed67a49ea3e29330eb7efdd050d4
Sasha Kuksovsky <specialforicq@gmail.com>
2022-01-28 14:31:00 +0300

    comment edits

--- src/skuksovsky/Lab3/Task3.java
+++ src/skuksovsky/Lab3/Task3.java
@@ -10,12 +10,12 @@ public class Task3 {
         scanner.close();
 
         String[] inputNumberArray = String.valueOf(inputNumber).split("");
-        int spaceIndex = inputNumberArray.length % 3; // место, где должен быть вставлен первый пробел
+        int spaceIndex = inputNumberArray.length % 3; // индекс, где должен быть вставлен первый пробел
         for (int i = 0; i < inputNumberArray.length; i++) {
             if (i != spaceIndex) {
                 System.out.print(inputNumberArray[i]);
             } else {
-                System.out.print(spaceIndex != 0 ? " " + inputNumberArray[i] : inputNumberArray[i]); // убираем ноль для нулевого индекса
+                System.out.print(spaceIndex != 0 ? " " + inputNumberArray[i] : inputNumberArray[i]); // вставляем ноль в нужном месте, исключение для нулевого индекса
                 spaceIndex +=3; // определяем следующий индекс, где должен быть вставлен пробел
             }
         }
