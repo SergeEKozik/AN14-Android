7d5429d86b3912a7bc8f49b0cbf5067c1276a579
ElenaVeshtard <h7692306@yandex.ru>
2022-01-28 22:52:41 +0300

    Task AN2: Tasks 3.

---
+++ src/eveshtard/TaskAN2/Task3.java
@@ -0,0 +1,27 @@
+package eveshtard.TaskAN2;
+//Дано целое число. Если оно является положительным, то прибавить к нему 1.
+// Если отрицательным, то вычесть из него 2.
+// Если нулевым, то заменить его на 10. Вывести полученное число.
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter number, please.");
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        System.out.println(zeroComparison(firstNumber));
+    }
+   
+    private static int zeroComparison(int d) {
+        if (d > 0) {
+            return d + 1;
+        } else if (d < 0) {
+            return d - 2;
+        } else {
+            return 10;
+        }
+    }
+}
+
