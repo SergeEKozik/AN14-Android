34fac9c3532095fb5898cc9cfdeceb5849401e6c
ElenaVeshtard <h7692306@yandex.ru>
2022-01-29 19:30:49 +0300

    Lab3. TaskA1. TaskA2.

--- src/eveshtard/Lab3/TaskA1.java
+++ src/eveshtard/Lab3/TaskA1.java
@@ -1,23 +1,24 @@
 package eveshtard.Lab3;
 //1. Имеется целое число. Следует посчитать сумму цифр данного числа.
+
 import java.util.Scanner;
 
 public class TaskA1 {
     public static void main(String[] args) {
-
         System.out.println("Enter number, please.");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        String str = String.valueOf(number);
-        int sum = 0;
-        sum(number, sum);
+        int result = 0;
+        summa(number, result);
     }
 
-    public static void sum(int n, int sum) {
+    public static void summa(int n, int sum) {
         if (n != 0) {
             sum += n % 10;
-            sum(n / 10, sum);
-        } else System.out.println(sum);
+            summa(n / 10, sum);
+        } else {
+            System.out.println(sum);
+        }
     }
 }
 
