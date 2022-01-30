8de17d674bf71d264d73c452f9a3ad0a41e5d90c
ElenaVeshtard <h7692306@yandex.ru>
2022-01-30 20:03:49 +0300

    Lab3.TaskA3. Add accounting number.

--- src/eveshtard/Lab3/TaskA3.java
+++ src/eveshtard/Lab3/TaskA3.java
@@ -3,25 +3,35 @@ package eveshtard.Lab3;
 import java.util.Arrays;
 
 //Имеется целое число, следует вывести его в бухгалтерском формате.
-//Т.е. начиная справа каждые три позиции отделяются пробелом. Например число
+//Т.е. начиная справа каждые три позиции отделяются пробелом. Например, число
 //20023143 должно быть выведено как 20 023 143
 public class TaskA3 {
     public static void main(String[] args) {
         int number = 20023143;
-        int groupSize = 3;
-        int numberLength = (int) (Math.log10(number) + 1);
-        int[] numberArray = new int[numberLength];
-       summa(number, numberArray);
-
+        System.out.println(toAccountingNumber(number));
     }
 
-    public static void summa(int n, int[] numA) {
-
-        for (int i = 0; i < numA.length; i++) {
-            numA[numA.length - i - 1] = n % 10;
-            n = n / 10;
+    private static String toAccountingNumber(int n) {
+        int groupSize = 3;
+        int numberLength = String.valueOf(n).length();
+        int numStrLength = numberLength + (numberLength / groupSize);
+        String[] numStr = new String[numStrLength];
+        int count = 0;
+        for (int i = 0; i < numStrLength; i++) {
+            if (count == groupSize) {
+                count = 0;
+                numStr[numStrLength - i - 1] = " ";
+            } else {
+                numStr[numStrLength - i - 1] = String.valueOf(n % 10);
+                n = n / 10;
+                count++;
+            }
         }
-        System.err.println(Arrays.toString(numA));
+        String result = "";
+        for (String digit : numStr) {
+            result = result + digit;
+        }
+        return result;
     }
 
 }
