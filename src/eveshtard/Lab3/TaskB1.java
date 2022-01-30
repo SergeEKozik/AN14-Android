d0d856e8eea7d0a92a3304a09e5e097e351940df
ElenaVeshtard <h7692306@yandex.ru>
2022-01-30 15:06:01 +0300

    Lab3. TaskB1.

---
+++ src/eveshtard/Lab3/TaskB1.java
@@ -0,0 +1,21 @@
+package eveshtard.Lab3;
+
+import java.util.Scanner;
+
+//Создайте программу, вычисляющую факториал натурального числа n, которое пользователь введёт с клавиатуры.
+public class TaskB1 {
+    public static void main(String[] args) {
+        System.out.println("Enter number, please.");
+        Scanner scanner = new Scanner(System.in);
+        int factorialOfNaturalNumber = scanner.nextInt();
+        System.out.println(factorial(factorialOfNaturalNumber));
+    }
+
+    private static int factorial(int nn) {
+        int fact = 1;
+        for (int i = 1; i <= nn; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+}
