5d477618616f8b1d27af645dca0841cf793ac445
ElenaVeshtard <h7692306@yandex.ru>
2022-02-01 22:20:56 +0300

    Lab4.Task1. Task2. Entering strings.

--- src/eveshtard/Lab4/Task1.java
+++ src/eveshtard/Lab4/Task1.java
@@ -7,20 +7,32 @@ import java.io.InputStreamReader;
 //Ввести n строк с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.
 public class Task1 {
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Введите число строк:");
-            int countString=Integer.parseInt(reader.readLine()) ;
-            String veryShortString=null;
-            for (int i=0; i< countString; i++){
-                System.out.println("Введите строку номер " + (i+1));
-                String currentString = reader.readLine();
-                if (veryShortString==null || currentString.length()<veryShortString.length()){
-                    veryShortString=currentString;
-                }
-            }
-            System.out.println("Самая короткая строка = " + veryShortString);
-            System.out.println("Ее длина равна " + veryShortString.length());
-        }
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число строк:");
+        int numberOfLines = Integer.parseInt(reader.readLine());
+
+        String theShortestString = "";
+        String theLongestString = "";
+        for (int i = 0; i < numberOfLines; i++) {
+            System.out.println("Введите строку номер " + (i + 1));
+            String currentString = reader.readLine();
+            if (theShortestString.length() == 0 || currentString.length() < theShortestString.length()) {
+                theShortestString = currentString;
+            }
+            if (currentString.length() > theLongestString.length()) {
+                theLongestString = currentString;
+            }
+        }
+        System.out.println("Самая короткая строка = " + theShortestString);
+        System.out.println("Ее длина равна " + theShortestString.length());
+
+        System.out.println("Самая длинная строка = " + theLongestString);
+        System.out.println("Ее длина равна " + theLongestString.length());
+
+
+    }
 }
+
+
--- src/eveshtard/Lab4/Task2.java
+++ src/eveshtard/Lab4/Task2.java
@@ -1,16 +1,17 @@
 package eveshtard.Lab4;
 //Ввести n строк с консоли. Упорядочить и вывести строки в порядке возрастания (убывания) значений их длины.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Task2 {
 
-        public static void main(String[] args) {
-            String str = "80 80 40 100";
-            Scanner scanner = new Scanner(str);
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
 
-            System.out.println(a + b);
-        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число строк:");
+        int numberOfLines = Integer.parseInt(reader.readLine());
 
+    }
 }
