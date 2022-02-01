30b7e68381545f6c95f6c1e2a2221c2e7d8463ba
ElenaVeshtard <h7692306@yandex.ru>
2022-02-02 00:11:59 +0300

    Lab4.Task1. Task2. SortArray

--- src/eveshtard/Lab4/Task2.java
+++ src/eveshtard/Lab4/Task2.java
@@ -1,9 +1,9 @@
 package eveshtard.Lab4;
 //Ввести n строк с консоли. Упорядочить и вывести строки в порядке возрастания (убывания) значений их длины.
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Task2 {
 
@@ -12,6 +12,28 @@ public class Task2 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите число строк:");
         int numberOfLines = Integer.parseInt(reader.readLine());
+        String[] arrayString = new String[numberOfLines];
+        for (int i = 0; i < arrayString.length; i++) {
+            System.out.println("Введите строку номер " + (i + 1));
+            String currentString = reader.readLine();
+            arrayString[i] = currentString;
+        }
+        sortArray(arrayString);
 
+        for (int i = 0; i < arrayString.length; i++) {
+            System.out.println(arrayString[i]);
+        }
+    }
+//сортировка по возрастанию.
+    private static void sortArray(String[] arrayString) {
+        for (int i = 0; i < arrayString.length; i++) {
+            for (int j = i + 1; j < arrayString.length; j++) {
+                if (arrayString[i].length() > arrayString[j].length()) {
+                    String temp = arrayString[i];
+                    arrayString[i] = arrayString[j];
+                    arrayString[j] = temp;
+                }
+            }
+        }
     }
 }
