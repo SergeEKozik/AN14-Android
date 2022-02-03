630e872782ca09438021395efc44e33bb2ba7685
Korzykov Vadim <98262539+korzykov@users.noreply.github.com>
2022-02-03 15:30:40 +0300

    Complete Task1

--- src/vkorzykov/Lab4/Task1.java
+++ src/vkorzykov/Lab4/Task1.java
@@ -23,12 +23,25 @@ public class Task1 {
         for (int i = 0; i < strings.size(); i++) {
             if (strings.get(i).length() > maxStringLength) {
                 maxStringLength = strings.get(i).length();
-                maxStringName.delete(0,1000000);
+                maxStringName.delete(0,maxStringName.length());
                 maxStringName = maxStringName.append(strings.get(i));
+            }
+        }
+        int minStringLength = maxStringLength;
+        StringBuilder minStringName = new StringBuilder();
+        minStringName.append("Бесполезный текст");
+
+        for (int i = 0; i < strings.size(); i++) {
+            if (strings.get(i).length() < minStringLength) {
+                minStringLength = strings.get(i).length();
+                minStringName.delete(0,minStringName.length());
+                minStringName = minStringName.append(strings.get(i));
             }
         }
         System.out.println("Самая длинная строка: " + maxStringName);
         System.out.println("Количество символов(включая пробелы): " + maxStringLength);
+        System.out.println("Самая короткая строка: " + minStringName);
+        System.out.println("Количество символов(включая пробелы): " + minStringLength);
     }
 }
 
