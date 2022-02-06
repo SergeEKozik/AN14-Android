8c6250312b573f26b7190d8c742442e31cef4719
Vladimir Nazarov <tooldiamond2014@gmail.com>
2022-02-07 02:15:51 +0300

    Task A1 completed

---
+++ src/VNazarov/Lab05/TaskA1/IActions.java
@@ -0,0 +1,10 @@
+package VNazarov.Lab05.TaskA1;
+
+public interface IActions {
+    void playVideo();
+    void stopVideo();
+    void playMusic();
+    void stopMusic();
+    void charge();
+    void stopCharging();
+}
