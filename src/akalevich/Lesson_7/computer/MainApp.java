bc5d7f77c9608e2ebfb84f830959174dbe43a6bb
Alexandr Kalevich <98311174+AleksandrKalevich@users.noreply.github.com>
2022-02-17 15:21:31 +0300

    Lesson 7 Computer Fix

--- src/akalevich/Lesson_7/computer/MainApp.java
+++ src/akalevich/Lesson_7/computer/MainApp.java
@@ -9,7 +9,7 @@ import akalevich.Lesson_7.computer.accessories.SSD.SamsungSSD512GB;
 
 public class MainApp {
 
-    public static final int ITERARIONS_FOR_COMPUTER_TEST = 8;
+    public static final int ITERATIONS_FOR_COMPUTER_TEST = 8;
 
     public static void main(String[] args) {
         Computer comp1 = new Computer(new AMDCPU(), new KingstonRAM16GB(), new SamsungSSD512GB());
@@ -21,6 +21,6 @@ public class MainApp {
     static void test(Computer comp) {
         System.out.println("");
         comp.showInfo();
-        for (int index = 0; index < ITERARIONS_FOR_COMPUTER_TEST; index++) { comp.turnON(); comp.turnOFF(); }
+        for (int index = 0; index < ITERATIONS_FOR_COMPUTER_TEST; index++) { comp.turnON(); comp.turnOFF(); }
     }
 }
