c5a4e52628e48ef754889572902c70a765737f36
Korzykov Vadim <98262539+korzykov@users.noreply.github.com>
2022-03-10 20:44:11 +0300

    Check 2.0

--- src/vkorzykov/Lab10/Check.java
+++ src/vkorzykov/Lab10/Check.java
@@ -3,5 +3,6 @@ package vkorzykov.Lab10;
 public class Check {
     public static void main(String[] args) {
         System.out.println("Чек на пуш");
+        System.out.println("Чек на пуш");
     }
 }
